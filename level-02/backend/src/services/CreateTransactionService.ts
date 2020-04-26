0ebb7b0575ba7c5c6fad51dde653d65b333e26aa
Melquisedec Felipe <melquisedecfelipe@gmail.com>
2020-04-26 14:49:03 -0300

    feat: add routes and service

---
+++ level-02/backend/src/services/CreateTransactionService.ts
@@ -0,0 +1,34 @@
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+class CreateTransactionService {
+  private transactionsRepository: TransactionsRepository;
+
+  constructor(transactionsRepository: TransactionsRepository) {
+    this.transactionsRepository = transactionsRepository;
+  }
+
+  public execute({ title, value, type }: Request): Transaction {
+    const validBalance = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > validBalance.total) {
+      throw Error('Transaction greater than cash value');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
+  }
+}
+
+export default CreateTransactionService;
